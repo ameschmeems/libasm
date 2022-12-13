329529a5e484cfe1f950fd1ca11eb9f9c75e2127
Kacper Pucylo <pucylek@wp.pl>
2022-12-13 19:28:10 +0100

    restored setting errno

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,7 +1,17 @@
 			global		_ft_read
+			extern		__errno_location
 
 			section		.text
 _ft_read:
-			mov			rax, 0x02000003
+			mov			rax, 0x02000003		; syscall read
 			syscall
+			cmp			rax, 0				; compare return value with 0
+			jl			error				; if less than 0, jump to error
+			ret
+error:
+			neg			rax					; get errno value
+			mov			rdi, rax			; save errno value
+			call		__errno_location	; save pointer to errno in rax
+			mov			[rax], rdi			; put errno value in errno address
+			mov			rax, -1				; set return value to -1
 			ret
--- src/ft_write.s
+++ src/ft_write.s
@@ -2,6 +2,15 @@
 
 			section		.text
 _ft_write:
-			mov			rax, 0x02000004
+			mov			rax, 0x02000004		; syscall write
 			syscall
+			cmp			rax, 0				; compare return value with 0
+			jl			error				; if less than 0, jump to error
+			ret
+error:
+			neg			rax					; get errno value
+			mov			rdi, rax			; save errno value
+			call		__errno_location	; save pointer to errno in rax
+			mov			[rax], rdi			; put errno value in errno address
+			mov			rax, -1				; set return value to -1
 			ret
